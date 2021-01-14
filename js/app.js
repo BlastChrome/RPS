ad64382dff621d36a3e12803bcb5ebbb8930fabc
nicholas jackson <nicdogg360@gmail.com>
2021-01-14 15:51:54 -0800

    main game logic done

--- js/app.js
+++ js/app.js
@@ -1,60 +1,153 @@
 const MIN = 1; 
-const MAX = 3;  
+const MAX = 3;   
+
+
+
+
+
+let cardDeck = document.querySelectorAll(".card");  
+let resetBtn = document.querySelector(".reset_btn");
+// let computerChoice = '';
+
+let win = 0; 
+let loss = 0; 
+
+let playerScore = document.querySelector(".win");  
+let computerScore = document.querySelector(".loss"); 
+let display = document.querySelector(".vs"); 
+
+
+function graphicUpdateRock(choice){
+    if(choice == "Rock"){
+        console.log("draw");  
+        display.innerHTML = "DRAW"; 
+    } else if(choice == "Paper"){
+        console.log("lose");  
+        lossUp(); 
+        computerScore.innerHTML = loss; 
+        display.innerHTML = "LOST"; 
+    } else{
+        console.log("win"); 
+        winUp();
+        playerScore.innerHTML = win; 
+        display.innerHTML = "WIN"; 
+        
+    } 
+
+}  
+function graphicUpdatePaper(choice){
+    if(choice == "Paper"){
+        console.log("draw");  
+        display.innerHTML = "DRAW";
+    } else if(choice == "Scissors"){
+        console.log("lose");  
+        lossUp(); 
+        computerScore.innerHTML = loss; 
+        display.innerHTML = "LOST"; 
+    } else{
+        console.log("win"); 
+        winUp();
+        playerScore.innerHTML = win; 
+        display.innerHTML = "WIN"; 
+        
+    } 
+}
+function graphicUpdateScissors(choice){
+    if(choice == "Scissors"){
+        console.log("draw");  
+        display.innerHTML = "DRAW";
+    } else if(choice == "Rock"){
+        console.log("lose");  
+        lossUp(); 
+        computerScore.innerHTML = loss; 
+        display.innerHTML = "LOST"; 
+    } else{
+        console.log("win"); 
+        winUp();
+        playerScore.innerHTML = win; 
+        display.innerHTML = "WIN"; 
+        
+    } 
+}
+function winUp(){
+    win++;
+} 
+
+function lossUp(){
+    loss++;
+}
+
+function reset(){
+    resetBtn.addEventListener('click', function(){
+        win = 0; 
+        loss = 0; 
+        display.innerHTML = "VS";   
+        playerScore.innerHTML = "0";
+        computerScore.innerHTML = "0";
+        console.log("reset");
+    })
+}
+
+function matchSet(){
+    
+}
+
+// handles the main game logic
+function cardSelect(){  
+   
+    cardDeck.forEach(function(card){ 
+            card.addEventListener('click', function(){    
+
+                // Ends the game after 5 rounds
+                if(win >= 5 || loss >= 5){
+                    console.log("Game end!") 
+                    return true;
+                }
+                // the computer makes it's selection at the same time as the player 
+                let computerChoice = computerPlay(); 
+                if(card.className == "rock card"){   
+                    console.log(`Rock vs ${computerChoice}`)  
+                    graphicUpdateRock(computerChoice); 
+                } else if(card.className == "paper card"){   
+                    console.log(`Paper vs ${computerChoice}`)  
+                    graphicUpdatePaper(computerChoice); 
+                } else{
+                    console.log(`Scissors vs ${computerChoice}`)  
+                    graphicUpdateScissors(computerChoice); 
+                }
+                console.log(`${win} | ${loss}`)
+            })
+        
+    }) 
+}
 
 // Generates random number(1-3). 1.Rock 2.Paper 3.Scissors
 function randomNumber(min,max){
     return Math.floor(Math.random() * max) + min  
 }
 
-
 // Uses the random number function to determine computer choice.
 function computerPlay(){ 
     let choice = randomNumber(MIN,MAX);  
-    if(choice === 1) return 'Rock';  
+    if(choice === 1) return "Rock";  
     else if(choice === 2) return 'Paper'; 
     else return 'Scissors';
 }  
 
-// takes input from the user
-function playerPlay(){
-    choice = prompt("Please Enter R P or S");  
+// Handles the game logic
+function game(){ 
+     cardSelect();    
+     reset();
     
-    if(choice === 'r' || choice === 'R') return 'Rock';  
-    else if (choice === 'p' || choice === 'P') return 'Paper';
-    else if (choice === 's' || choice === 'S') return 'Scissors';
-    
-}
 
-function game(){  
-    let redo = false; 
-    let win = 0; 
-    let loss = 0;
-    do{
-        let computer = computerPlay();
-        let player = playerPlay(); 
-    
-    
-        console.log(`player: ${player} CPU: ${computer}  ${win}/${loss} `);
-    
-        if(player === computer) console.log("Draw");  
-        else if(player === "Rock" && computer == "Paper" ) {console.log("You Lose"); loss++;}
-        else if(player === "Paper" && computer == "Rock" ) { console.log("You Win"); win++;}
-    
-        else if(player === "Scissors" && computer == "Rock" ){ console.log("You Lose"); loss++}
-        else if(player === "Rock" && computer == "Scissors" ){ console.log("You Win"); win++;} 
-    
-        else if(player === "Paper" && computer == "Scissors" ){ console.log("You Loe"); loss++;}  
-        else if(player === "Scissors" && computer == "Paper" ){ console.log("You Win"); win++;} 
-
-        let playAgain = prompt("Play again?(Y/N): "); 
-        if(playAgain === 'Y' || playAgain === 'y') redo = true;
-        else redo = false; 
-
-    } while( redo === true);
-}  
-
+} 
 
 game();
+
+
+
+
+
 
 
 
